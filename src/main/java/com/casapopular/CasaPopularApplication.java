0818c2d0e46da09a7829d428375b70be47e446b5
MilenaTavaresRoas <milenatavares@digix.com.br>
2023-11-10 09:40:54 -0300

    Inicializando o projeto

---
+++ src/main/java/com/casapopular/CasaPopularApplication.java
@@ -0,0 +1,13 @@
+package com.casapopular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaPopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaPopularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casapopular/CasaPopularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casapopular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaPopularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
