ada3c25bc6f00a13c44fbc36b440424d075706b0
MilenaTavaresRoas <milenatavares@digix.com.br>
2023-11-14 18:14:05 -0300

    Configurar banco de dados mysql

--- src/main/java/com/casapopular/CasaPopularApplication.java
+++ src/main/java/com/casapopular/CasaPopularApplication.java
@@ -2,8 +2,9 @@ package com.casapopular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CasaPopularApplication {
 
 	public static void main(String[] args) {
