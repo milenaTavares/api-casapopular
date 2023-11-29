4767c1847362b26814dbae844646ab1daa936cca
MilenaTavaresRoas <milenatavares@digix.com.br>
2023-11-29 15:45:22 -0300

    Criar classe familia

---
+++ src/main/java/com/casapopular/domain/Familia.java
@@ -0,0 +1,39 @@
+package com.casapopular.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Familia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "familia_id")
+    private List<Pessoa> membros;
+
+    public Familia(List<Pessoa> membros) {
+        this.membros = membros;
+    }
+
+    public Integer obterQuantidadeDeMembrosMaioresDeIdade() {
+        return this.membros.stream().filter(Pessoa::ehMaiorDeIdade).toList().size();
+    }
+
+    public Double obterRendaTotal() {
+        return this.membros.stream().mapToDouble(Pessoa::getRenda).sum();
+    }
+}
---
+++ src/test/java/com/casapopular/domain/FamiliaTest.java
@@ -0,0 +1,57 @@
+package com.casapopular.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+class FamiliaTest {
+
+    @Test
+    void deveCriarUmaFamiliaComOsMembrosInformados() {
+        Pessoa primeiroMembro = new Pessoa("Maria", 45, 2000.50);
+        Pessoa segundoMembro = new Pessoa("Joao", 47, 4250.00);
+        Pessoa terceiroMembro = new Pessoa("José", 19, 1500.00);
+        List<Pessoa> membrosDaFamilia = Arrays.asList(primeiroMembro, segundoMembro, terceiroMembro);
+
+        Familia familia = new Familia(membrosDaFamilia);
+
+        Assertions.assertThat(familia.getMembros()).extracting(Pessoa::getId)
+                .containsExactlyInAnyOrder(primeiroMembro.getId(), segundoMembro.getId(), terceiroMembro.getId());
+        Assertions.assertThat(familia.getMembros()).extracting(Pessoa::getNome)
+                .containsExactlyInAnyOrder(primeiroMembro.getNome(), segundoMembro.getNome(), terceiroMembro.getNome());
+        Assertions.assertThat(familia.getMembros()).extracting(Pessoa::getIdade)
+                .containsExactlyInAnyOrder(primeiroMembro.getIdade(), segundoMembro.getIdade(), terceiroMembro.getIdade());
+        Assertions.assertThat(familia.getMembros()).extracting(Pessoa::getRenda)
+                .containsExactlyInAnyOrder(primeiroMembro.getRenda(), segundoMembro.getRenda(), terceiroMembro.getRenda());
+    }
+
+    @Test
+    void deveObterAQuantidadeDeMembrosDaFamiliaMaioresDeIdade() {
+        Pessoa primeiroMembro = new Pessoa("Maria", 45, 2000.50);
+        Pessoa segundoMembro = new Pessoa("José", 18, 0000.00);
+        Pessoa terceiroMembro = new Pessoa("Joana", 15, 500.00);
+        List<Pessoa> membrosDaFamilia = Arrays.asList(primeiroMembro, segundoMembro, terceiroMembro);
+        Familia familia = new Familia(membrosDaFamilia);
+
+        Integer quantidadeMembrosMaioresDeIdade = familia.obterQuantidadeDeMembrosMaioresDeIdade();
+
+        Assertions.assertThat(quantidadeMembrosMaioresDeIdade).isEqualTo(2);
+    }
+
+    @Test
+    void deveObterARendaTotalDaFamilia() {
+        Pessoa primeiroMembro = new Pessoa("Maria", 45, 2000.50);
+        Pessoa segundoMembro = new Pessoa("Joao", 47, 4250.00);
+        Pessoa terceiroMembro = new Pessoa("José", 19, 0000.00);
+        Pessoa quartoMembro = new Pessoa("Joana", 15, 500.00);
+        List<Pessoa> membrosDaFamilia = Arrays.asList(primeiroMembro, segundoMembro, terceiroMembro, quartoMembro);
+        Familia familia = new Familia(membrosDaFamilia);
+        Double rendaEsperada = 6750.50;
+
+        Double rendaTotal = familia.obterRendaTotal();
+
+        Assertions.assertThat(rendaTotal).isEqualTo(rendaEsperada);
+    }
+}
