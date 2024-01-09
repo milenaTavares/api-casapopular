7538d787ca817ffb896d97ea3ac5bd51dedb13e0
MilenaTavaresRoas <milenatavares@digix.com.br>
2024-01-09 17:33:57 -0300

    Criar fabrica de selecao

---
+++ src/main/java/com/casapopular/dominio/selecao/SelecaoFabrica.java
@@ -0,0 +1,54 @@
+package com.casapopular.dominio.selecao;
+
+import com.casapopular.adaptador.persistencia.FamiliaRepositorio;
+import com.casapopular.dominio.familia.Familia;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SelecaoFabrica {
+
+    private final FamiliaRepositorio familiaRepositorio;
+    private final CalculaPontuacao calculaPontuacao;
+
+    @Autowired
+    public SelecaoFabrica(FamiliaRepositorio familiaRepositorio) {
+        this.familiaRepositorio = familiaRepositorio;
+        this.calculaPontuacao = new CalculaPontuacao();
+    }
+
+    public Selecao fabricar(Integer numeroDeFamiliasSelecionadas) {
+        List<Familia> familiasCadastradas = familiaRepositorio.findAll();
+        validarFamilias(familiasCadastradas);
+        List<FamiliaPontuada> familiasPontuadas = obterFamiliasPontuadas(familiasCadastradas);
+        List<FamiliaPontuada> familiasSelecionadas = selecionarFamilias(familiasPontuadas, numeroDeFamiliasSelecionadas);
+
+        return criarSelecao(familiasSelecionadas);
+    }
+
+    private void validarFamilias(List<Familia> familias) {
+        throw new RuntimeException("Não existem famílias cadastradas.");
+    }
+
+    private List<FamiliaPontuada> obterFamiliasPontuadas(List<Familia> familiasCadastradas) {
+        return familiasCadastradas.stream().map(calculaPontuacao::calcular).collect(Collectors.toList());
+    }
+
+    private List<FamiliaPontuada> selecionarFamilias(List<FamiliaPontuada> familiasPontuadas, Integer numeroDeFamiliasSelecionadas) {
+        return familiasPontuadas.stream()
+                .sorted(Comparator.comparingInt(FamiliaPontuada::getPontuacao).reversed())
+                .limit(numeroDeFamiliasSelecionadas)
+                .collect(Collectors.toList());
+    }
+
+    private Selecao criarSelecao(List<FamiliaPontuada> familiasSelecionadasComPontuacao) {
+        List<FamiliaSelecionada> familiaSelecionadas = familiasSelecionadasComPontuacao.stream()
+                .map(FamiliaSelecionada::criar)
+                .toList();
+        return new Selecao(familiaSelecionadas);
+    }
+}
